5666c7349c08801875ad45dd5f4eed7602bc5405
Logan Hochwald <loganhochwald@gmail.com>
2024-08-24 15:24:22 -0500

    💻: render index.html

--- Amalgamoria/main.py
+++ Amalgamoria/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="Views")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
