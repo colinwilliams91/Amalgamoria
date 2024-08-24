777e877a305280b50135aae0c2f50f8b7cc34153
Logan Hochwald <loganhochwald@gmail.com>
2024-08-24 17:51:52 -0500

    :sparkles:: move gemini router to controller file

--- Amalgamoria/main.py
+++ Amalgamoria/main.py
@@ -1,9 +1,10 @@
 import sys
 import os
-from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from Controllers.gemini import model
+from fastapi import FastAPI, Request
+from Controllers.gemini import router as gemini_router
 
 # Add the parent directory to the path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -11,11 +12,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app = FastAPI()
 templates = Jinja2Templates(directory="Views")
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# Include the router for /gemini routes
+app.include_router(gemini_router, prefix="/gemini")
 
-@app.post("/")
-async def handle_input(request: Request, user_input: str = Form(...)):
-    response_data = model.generate_content(user_input)
-    return templates.TemplateResponse("index.html", {"request": request, "response_data": response_data.text})
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
