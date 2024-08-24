0410af1a14a49460dd1f3b6c688e1f53274c7816
Logan Hochwald <loganhochwald@gmail.com>
2024-08-24 15:12:50 -0500

    :memo:: install fastapi and uninstall flask

--- Amalgamoria/main.py
+++ Amalgamoria/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
