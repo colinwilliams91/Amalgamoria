44b93568dc94a47a7879701944d122e8e71a1428
kalypsohoman <catcatmcgee@gmail.com>
2024-08-25 16:17:30 -0500

    :sparkles: Create modifier table

---
+++ Tables/modifiers.py
@@ -0,0 +1,48 @@
+chat_modifiers = [
+    {
+        "name": "silence",
+        "description": "user's messages are not being sent",
+        "weight": -4,
+    }, {
+        "name": "amalgamation",
+        "description": "multiple user text messages are arbitrarily combined",
+        "weight": -2,
+    }, {
+        "name": "scramble",
+        "description": "user's messages are not being sent",
+        "weight": -3,
+    }
+]
+
+scene_modifiers = [
+    {
+        "name": "happy 3",
+        "description": "Make the scenes full of adventure and grandiose joy",
+        "weight": -3,
+    }, {
+        "name": "happy 2",
+        "description": "Make the scenes happy and fun",
+        "weight": -3,
+    }, {
+        "name": "happy 1",
+        "description": "Make the scenes slightly airy and dramatic but carefree and adventurous",
+        "weight": -3,
+    }, {
+        "name": "neutral",
+        "description": "",
+        "weight": -3,
+    }, {
+        "name": "dark 1",
+        "description": "Make the scenes slightly scarier",
+        "weight": -1,
+    }, {
+        "name": "dark 2",
+        "description": "Make the scenes scary and disturbing",
+        "weight": -2,
+    }, {
+        "name": "dark 3",
+        "description": "Make the scenes horrific and disturbing with the possibility for violent descriptions",
+        "weight": -3,
+    }
+    
+]
