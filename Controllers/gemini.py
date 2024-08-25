a572a6b92f8830aaf0d87438c02bb27b4beb1139
kalypsohoman <catcatmcgee@gmail.com>
2024-08-25 13:22:47 -0500

    :sparkle: Use prompt_generator instance

--- Controllers/gemini.py
+++ Controllers/gemini.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from Controllers.prompt import Prompt
 
 load_dotenv()
 
@@ -10,12 +11,16 @@ gemini_api = os.getenv("GEMINI_API")
 genai.configure(api_key=gemini_api)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+
 router = APIRouter()
+prompt_generator = Prompt()
 
 # Takes form data (user_input) as input.
 # The Form(...) part indicates that user_input is expected to come from a form submission and is required.
+# user_input is not used for not, but keeping it in incase its helpful later
 @router.post("/gemini")
 async def post_gemini(user_input: str = Form(...)):
-    response_data = model.generate_content(user_input)
+    prompt = prompt_generator.create_prompt()
+    response_data = model.generate_content(prompt)
     # Return the generated content as a JSON response
     return JSONResponse(content={"response": response_data.text})
